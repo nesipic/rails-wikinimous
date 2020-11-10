0cb93b918b33d3f99cf45c3a5676703c3ea035fb
NedijkoSipic <nesipic@hotmail.com>
2020-11-11 00:12:06 +0100

    works, style need more work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+  article = Article.create(
+    title: Faker::Address.city,
+    content: Faker::Address.street_name.to_s,
+  )
+end
